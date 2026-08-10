691c922d7fd311cdf409cb791252592adeefdf6e
Leo Cheung <leozhang621@gmail.com>
2026-08-10 18:41:39 +0800

    fix(devtools): move TanStackDevtools position to bottom-left

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -61,7 +61,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         {children}
         <TanStackDevtools
           config={{
-            position: 'bottom-right',
+            position: 'bottom-left',
           }}
           plugins={[
             {
