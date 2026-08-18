c3a7887e08f1013de42d5d0817fa9521d19d9cec
Leo Cheung <leozhang621@gmail.com>
2026-08-18 11:07:57 +0800

    refactor(seeder): simplify user seeder syntax for better readability

--- apps/api/database/seeders/user_seeder.ts
+++ apps/api/database/seeders/user_seeder.ts
@@ -6,12 +6,11 @@ import User from '#models/user';
 export default class extends BaseSeeder {
   async run() {
     await User.updateOrCreateMany('username', [
-      {
-        username: 'admin',
-        email: 'leozhang621@gmail.com',
-        id: uuidv7(),
-        password: 'naliankeji',
-      },
+      { username: 'admin', email: 'leozhang621@gmail.com', id: uuidv7(), password: 'naliankeji' },
+      { username: 'user1', email: 'user1example@gmail.com', id: uuidv7(), password: 'naliankeji' },
+      { username: 'user2', email: 'user2example@gmail.com', id: uuidv7(), password: 'naliankeji' },
+      { username: 'user3', email: 'user3example@gmail.com', id: uuidv7(), password: 'naliankeji' },
+      { username: 'user4', email: 'user4example@gmail.com', id: uuidv7(), password: 'naliankeji' },
     ]);
   }
 }
