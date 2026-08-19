29c9fbcc67055ac4d5c498516f6bfb1dd00b1e19
Leo Cheung <leozhang621@gmail.com>
2026-08-19 11:50:16 +0800

    feat(video): update grid layout in VideoList component for improved responsiveness

--- apps/web/src/features/video/list/video-list.tsx
+++ apps/web/src/features/video/list/video-list.tsx
@@ -3,7 +3,7 @@ import { VideoItem } from './video-item';
 
 export function VideoList({ videos, onEdit }: { videos: Array<Video>; onEdit: (video: Video) => void }) {
   return (
-    <div className="grid w-full grid-cols-6 gap-4 px-4 pb-4 pt-16">
+    <div className="grid w-full grid-cols-2 gap-4 px-4 pb-4 pt-16 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
       {videos.map((video) => (
         <VideoItem key={video.id} video={video} onEdit={() => onEdit(video)} />
       ))}
